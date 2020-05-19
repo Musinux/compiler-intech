6b11e9a4f2684e33151bcdef7356ae314dab8538
Louis Cherel <cherel.louis@gmail.com>
2020-05-19 17:30:29 +0200

    src/ast.c: add all ast_new_* functions

--- src/ast.c
+++ src/ast.c
@@ -9,9 +9,17 @@
 #include "ast.h"
 #include "utils.h"
 
+char *copy_name (char *name)
+{
+  size_t len = (strlen(name) + 1);
+  char *out = malloc(sizeof(char) * len);
+  strncpy(out, name, len);
+  return out;
+}
+
 ast_t *ast_new_integer (long val)
 {
-  ast_t *ast = malloc(sizeof(ast_t));
+  ast_t * ast = malloc(sizeof(ast_t));
   ast->type = AST_INTEGER;
   ast->integer = val;
   return ast;
@@ -19,11 +27,101 @@ ast_t *ast_new_integer (long val)
 
 ast_t *ast_new_binary (ast_binary_e op, ast_t *left, ast_t *right)
 {
-  ast_t *ast = malloc(sizeof(ast_t));
+  ast_t * ast = malloc(sizeof(ast_t));
   ast->type = AST_BINARY;
   ast->binary.op = op;
   ast->binary.left = left;
   ast->binary.right = right;
+  return ast;
+}
+
+ast_t *ast_new_variable (char *name, int type) {
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_VARIABLE;
+  ast->var.name = copy_name(name);
+  ast->var.type = type;
+  return ast;
+}
+
+ast_t *ast_new_unary (char op, ast_t *operand)
+{
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_UNARY;
+  ast->unary.op = op;
+  ast->unary.operand = operand;
+  return ast;
+}
+
+ast_t *ast_new_function (char *name, int return_type, ast_list_t *params, ast_list_t *stmts)
+{
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_FUNCTION;
+  ast->function.name = copy_name(name);
+  ast->function.return_type = return_type;
+  ast->function.params = params;
+  ast->function.stmts = stmts;
+  return ast;
+}
+
+ast_t *ast_new_fncall (char *name, ast_list_t *args)
+{
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_FNCALL;
+  ast->call.name = copy_name(name);
+  ast->call.args = args;
+  return ast;
+}
+
+ast_t *ast_new_comp_stmt (ast_list_t *stmts)
+{
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_COMPOUND_STATEMENT;
+  ast->compound_stmt.stmts = stmts;
+  return ast;
+}
+
+ast_t *ast_new_declaration (ast_t *lvalue, ast_t *rvalue)
+{
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_DECLARATION;
+  ast->assignment.lvalue = lvalue;
+  ast->assignment.rvalue = rvalue;
+  return ast;
+}
+
+ast_t *ast_new_assignment (ast_t *lvalue, ast_t *rvalue)
+{
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_ASSIGNMENT;
+  ast->assignment.lvalue = lvalue;
+  ast->assignment.rvalue = rvalue;
+  return ast;
+}
+
+ast_t *ast_new_condition (ast_t *condition, ast_t *valid, ast_t *invalid)
+{
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_CONDITION;
+  ast->branch.condition = condition;
+  ast->branch.valid = valid;
+  ast->branch.invalid = invalid;
+  return ast;
+}
+
+ast_t *ast_new_loop (ast_t *condition, ast_t *stmt)
+{
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_LOOP;
+  ast->loop.condition = condition;
+  ast->loop.stmt = stmt;
+  return ast;
+}
+
+ast_t *ast_new_return (ast_t *expr)
+{
+  ast_t * ast = malloc(sizeof(ast_t));
+  ast->type = AST_RETURN;
+  ast->ret.expr = expr;
   return ast;
 }
 
@@ -44,6 +142,48 @@ char *ast_get_ret_type (ast_t *ast) {
     return "rien";
   }
   return "";
+}
+
+ast_list_t *ast_list_new_node (ast_t *elem)
+{
+  ast_list_t *node = malloc(sizeof(ast_list_t));
+  node->elem = elem;
+  node->next = NULL;
+  return node;
+}
+
+ast_list_t *ast_list_add (ast_list_t **list, ast_t *elem)
+{
+  assert(elem != NULL);
+  assert(list != NULL);
+
+  while (*list)
+    list = &(*list)->next;
+
+  *list = ast_list_new_node(elem);
+  return *list;
+}
+
+int ast_binary_priority (ast_t *ast)
+{
+  if (ast == NULL) return 0;
+  if (ast->type != AST_BINARY) return 100;
+
+  switch (ast->binary.op) {
+    case AST_BIN_OR: return 10;
+    case AST_BIN_AND: return 10;
+    case AST_BIN_GTE: return 20;
+    case AST_BIN_GT: return 20;
+    case AST_BIN_LTE: return 20;
+    case AST_BIN_LT: return 20;
+    case AST_BIN_DIFF: return 20;
+    case AST_BIN_EQ: return 20;
+    case AST_BIN_PLUS: return 30;
+    case AST_BIN_MINUS: return 30;
+    case AST_BIN_MULT: return 40;
+    case AST_BIN_DIV: return 40;
+    default: return 0;
+  }
 }
 
 char *ast_binary_to_string (ast_binary_e op)
