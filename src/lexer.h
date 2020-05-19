544ced2ea32de69d1fd88c157facf194d720212c
Louis Cherel <cherel.louis@gmail.com>
2020-05-19 08:42:28 +0200

    lexer.h: fix prototypes

--- src/lexer.h
+++ src/lexer.h
@@ -9,23 +9,25 @@
 // bool isnumber (char chr);
 // bool isop (char chr);
 
-// void lexem_assert_simplechar (buffer_t *buffer, char chr, char *msg);
-// void lexem_assert_twopoints (buffer_t *buffer, char *msg);
-// void lexem_assert_semicolon (buffer_t *buffer, char *msg);
-// void lexem_assert_openbrace (buffer_t *buffer, char *msg);
-// void lexem_assert_closebrace (buffer_t *buffer, char *msg);
-// void lexem_assert_openbracket (buffer_t *buffer, char *msg);
-// void lexem_assert_closebracket (buffer_t *buffer, char *msg);
-// void lexem_assert_equalsign (buffer_t *buffer, char *msg);
-// void lexem_assert_blank (buffer_t *buffer, char *msg);
+// void lexer_assert_simplechar (buffer_t *buffer, char chr, char *msg);
+// void lexer_assert_twopoints (buffer_t *buffer, char *msg);
+// void lexer_assert_semicolon (buffer_t *buffer, char *msg);
+// void lexer_assert_openbrace (buffer_t *buffer, char *msg);
+// void lexer_assert_closebrace (buffer_t *buffer, char *msg);
+// void lexer_assert_openbracket (buffer_t *buffer, char *msg);
+// void lexer_assert_closebracket (buffer_t *buffer, char *msg);
+// void lexer_assert_equalsign (buffer_t *buffer, char *msg);
+// void lexer_assert_blank (buffer_t *buffer, char *msg);
 
-/** buf_getalphanum_rollback mallocs and rollbacks after read. **/
-// char *lexem_getalphanum_rollback (buffer_t *buffer);
-/** buf_getalphanum mallocs and rollbacks only if nothing was parsed. **/
-// char *lexem_getalphanum (buffer_t *buffer);
-/** buf_getop mallocs and rollbacks only if nothing was parsed. **/
-// char *lexem_getop (buffer_t *buffer);
-/** buf_getnumber mallocs and rollbacks only if nothing was parsed. **/
-// char *lexem_getnumber (buffer_t *buffer);
+/** lexer_getalphanum_rollback mallocs and rollbacks after read. **/
+// char *lexer_getalphanum_rollback (buffer_t *buffer);
+/** lexer_getalphanum mallocs and rollbacks only if nothing was parsed. **/
+// char *lexer_getalphanum (buffer_t *buffer);
+/** lexer_getalpha mallocs and rollbacks only if nothing was parsed. **/
+// char *lexer_getalpha (buffer_t *buffer);
+/** lexer_getop mallocs and rollbacks only if nothing was parsed. **/
+// char *lexer_getop (buffer_t *buffer);
+/** lexer_getnumber mallocs and rollbacks only if nothing was parsed. **/
+// char *lexer_getnumber (buffer_t *buffer);
 
 #endif /* ifndef LEXER_H */
